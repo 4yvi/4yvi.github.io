a6fce6a381a5c77a5ca9bd57b57e5302d1cf4e3f
4yvi <gadruit@gmail.com>
2016-12-23 20:38:26 +0300

    add images end edit index texts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	skel.breakpoints({
@@ -16,6 +10,14 @@
 	});
 
 	$(function() {
+        function getRandomArbitary(min, max)
+        {
+            return Math.round(Math.random() * (max - min) + min);
+        }
+        var bg = getRandomArbitary(1,37);
+        //$('#bg::after').css('background-image', '/images/lib/' + bg + '.jpg');
+        console.log(getRandomArbitary(1,37));
+        $('#bg span').attr('style','background-image: url(/images/lib/3.jpg);');
 
 		var	$window = $(window),
 			$body = $('body'),
